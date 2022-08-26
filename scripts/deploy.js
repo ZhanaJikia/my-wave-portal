b14e4ced2080c72eff61891cfe8c636eeeed2f5a
ZhanaJikia <zhana.jikia.1@btu.edu.ge>
2022-08-26 17:49:28 +0400

    feat: contract local deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    console.log("deploying with account: ", deployer.address);
+    console.log("Account Balance: ", accountBalance.toString());
+
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // Hardhat Runtime Environment
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("Wave portal adress: ", waveContract.address);
+}
+
+const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+
+runMain();
---
+++ scripts/run.js
@@ -0,0 +1,26 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // Hardhat Runtime Environment
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let wavesCount;
+    wavesCount = await waveContract.getTotalWaves();
+    let wave = await waveContract.connect(randomPerson).wave();
+    await wave.wait();
+    wavesCount = await waveContract.getTotalWaves();
+  };
+
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0); // exit Node process without error
+    } catch (error) {
+      console.log(error);
+      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    }
+  };
+
+  runMain();
